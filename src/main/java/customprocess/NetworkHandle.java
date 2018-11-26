41aeaf5847ed848e132f2a933dd991cd6c6feebb
Harkishen <harkishensingh@hotmail.com>
2018-11-27 01:48:52 +0530

    finish network stats all processes

--- src/main/java/customprocess/NetworkHandle.java
+++ src/main/java/customprocess/NetworkHandle.java
@@ -52,7 +52,6 @@ public class NetworkHandle {
     public List<Object> netstatPID(String pid) throws IOException {
         Process netstats = Runtime.getRuntime().exec("netstat -tanp | grep "+(pid));
         Scanner SS = new Scanner(netstats.getInputStream());
-        System.out.println("from netstatas");
         String line="", sentence="";
         Boolean exit=false;
         while (true) {
@@ -76,9 +75,6 @@ public class NetworkHandle {
             else if ((n==3|| n==15 || n == 32))
                 headings.add(headers[n].trim()+" "+headers[n+1].trim());
         }
-        System.out.println("Headings below");
-        System.out.println(headings);
-        System.out.println(aa.length);
         List<Object> processObjects = new ArrayList<Object>();
         for (int j=2; j< aa.length; j++){ // first two being the headings
             Map<String, String> abc = new LinkedHashMap<String, String>();
@@ -106,12 +102,63 @@ public class NetworkHandle {
         return processObjects;
     }
 
+    public List<Object> netstatALL() throws IOException {
+        Process netstats = Runtime.getRuntime().exec("netstat -tanp");
+        Scanner SS = new Scanner(netstats.getInputStream());
+        String line="", sentence="";
+        Boolean exit=false;
+        while (true) {
+            try {
+                line = SS.nextLine();
+                if (line == null)
+                    break;
+                sentence += line + this.separator;
+            } catch (Exception e) {
+                exit=true;
+            } if (exit)
+                break;
+        }
+        String[] aa = sentence.split(this.separator);
+        String[] headers = aa[1].split(" ");
+        // making list of it
+        List<String> headings = new ArrayList<String>();
+        for (int n=0;n< headers.length; n++) {
+            if (!headers[n].trim().equals("") && !(n==3 || n ==15 || n==32) && !(n==4 || n==16 || n==33))
+                headings.add(headers[n].trim());
+            else if ((n==3|| n==15 || n == 32))
+                headings.add(headers[n].trim()+" "+headers[n+1].trim());
+        }
+        List<Object> processObjects = new ArrayList<Object>();
+        for (int j=2; j< aa.length; j++){ // first two being the headings
+            Map<String, String> abc = new LinkedHashMap<String, String>();
+            String[] b = aa[j].split(" ");
+            // filter empty cells from b
+            List<String> b_filtered = new ArrayList<String>();
+            int count = 0;
+            for (int x=0;x< b.length; x++) {
+                if (!b[x].trim().equals(""))
+                    b_filtered.add(b[x].trim());
+            }
+            if (b_filtered.size()==headings.size())
+                for (int k = 0; k < b_filtered.size(); k++)
+                    abc.put(headings.get(k).trim(), b_filtered.get(k).trim());
+            else
+                System.err.println("Size incompatible");
+            processObjects.add(abc);
+        }
+        return processObjects;
+    }
+
+
+
     public void runFunctionalities() throws SigarException {
         this.getNetworkCards();
         this.getNetworkCardsStats();
     }
 
+
+
     public static void main(String[] args) throws IOException {
-        System.out.print(new NetworkHandle().netstatPID(Integer.toString(2059)));
+        System.out.print(new NetworkHandle().netstatALL());
     }
 }
